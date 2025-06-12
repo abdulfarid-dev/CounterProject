5a069892d45478417dbefd13cadbbb77e7d237b8
Abdul Farid <farid995576@gmail.com~>
2025-06-12 21:48:13 +0530

    counter project

---
+++ src/Counter.jsx
@@ -0,0 +1,25 @@
+
+
+import { useState } from "react";
+export default function Counter(){
+     let[count,SetCount]=useState(10);
+    // let count =0;
+
+    function IncCounter(){
+        SetCount(count+1);
+        SetCount(count-1);
+        // console.log(count);
+        
+    }
+    return(
+        <>
+           <h1>State in react</h1>
+          <h3>count={count}</h3> 
+            <button onClick={IncCounter}>counter increment</button>
+            <button onClick={IncCounter}>counter increment</button>
+        </>
+
+    
+    );
+
+}
